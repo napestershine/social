43eb57147ecba67f7d9be750f8a5e4405f320c26
root <napestershine@gmail.com>
2016-08-24 08:59:51 +0530

    created dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,49 @@
-<h1>The Dashboard</h1>
+@extends('layouts.master)
+
+@section('content')
+    <section class="row new-post">
+        <div class="col-md-6 col-md-offset-3">
+            <header>
+                <h3>What do you have to say?</h3>
+                <form action="#">
+                    <div class="form-group">
+                        <textarea class="form-control" name="new-post" id="new-post" rows="5"
+                                  placeholder="Your Post"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Create Post</button>
+                </form>
+            </header>
+        </div>
+    </section>
+    <section class="row posts">
+        <div class="col-md-6 col-md-offset-3">
+            <header>
+                <h3>What other people say...</h3>
+            </header>
+            <article class="post">
+                <p>Spaces are the processors of the futile mystery. Not nirvana or hell, capture the shame.</p>
+                <div class="info">
+                    Posted by Max on 12 Feb 2016
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">disike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a> |
+                </div>
+            </article>
+            <article class="post">
+                <p>Spaces are the processors of the futile mystery. Not nirvana or hell, capture the shame.</p>
+                <div class="info">
+                    Posted by Max on 12 Feb 2016
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">disike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a> |
+                </div>
+            </article>
+        </div>
+    </section>
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="{{ URL::to('src/css/main.css') }}" type="text/css" rel="stylesheet">
 </head>
 <body>
 @include('includes.header')
