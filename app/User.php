420e393575015e550c20d07ba925700ebf6e90c5
root <napestershine@gmail.com>
2016-08-25 07:44:07 +0530

    created migration and model for posts, Added mapping between post and user, Created PostController

--- app/User.php
+++ app/User.php
@@ -9,4 +9,8 @@ class User extends Model implements Authenticatable
 {
     use \Illuminate\Auth\Authenticatable;
 
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
